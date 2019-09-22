60d683b7c8f39a0ac52d46b0ff82d9a734ab6d20
Noutai Wilfrid Cesaire <wilfrid-cesaire.noutai@etu.uni-orleans.fr>
2019-09-22 13:27:33 +0200

    fichiers sources

---
+++ src/main/java/immo/project/immoplus/ImmoplusApplication.java
@@ -0,0 +1,13 @@
+package immo.project.immoplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoplusApplication.class, args);
+	}
+
+}
